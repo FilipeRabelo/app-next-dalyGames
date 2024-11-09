25f8635d1198f47e882c31b00205d172339b6193
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2024-11-09 01:41:11 -0300

    app-next-dalyGames

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,15 @@ import { GamesProps } from '@/utils/types/game';
 import Image from "next/image";
 import Link from 'next/link';
 
-async function getDalyGames() {
+import { BsArrowRightSquare } from 'react-icons/bs';
 
+async function getDalyGames() {
   try {
 
-    const response = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=game_day`);
+    const response = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=game_day`, {
+      next: { revalidate: 320 }
+    });
+
     return response.json();
 
   } catch (err) {
@@ -21,25 +25,38 @@ export default async function Home() {
 
   // console.log(dalyGame);
 
-
   return (
     <main className="w-full">
       <Container>
 
         <h1 className="text-center font-bold text-xl mt-8 mb-5">
-          Separamos um jogo exclusivo para vc
+          Separamos um jogo exclusivo para você
         </h1>
 
         <Link href={`/game/${dalyGame.id}`}>
           <section className="w-full bg-black rounded-lg">
-           <Image
-            src={dalyGame.image_url}
-            alt={dalyGame.title}
-            priority={true}
-            quality={100}
-            width={100}
-            height={100}
-           />
+
+            <div className="w-full max-h-96 h-96 relative rounded-lg">
+
+              <div className="absolute z-20 bottom-0 p-3 flex justify-center items-center gap-2">
+                <p className="font-bold text-xl text-white">
+                  {dalyGame.title}
+                </p>
+                <BsArrowRightSquare size={24} color="#FFF" />
+              </div>
+
+              <Image
+                src={dalyGame.image_url}
+                alt={dalyGame.title}
+                priority={true}
+                quality={100}
+
+                fill={true} // preenchimento total
+                className="max-h-96 object-cover rounded-lg opacity-50 hover:opacity-100 transition-all duration-300"
+                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 44vw"
+              />
+            </div>
+
           </section>
         </Link>
 
