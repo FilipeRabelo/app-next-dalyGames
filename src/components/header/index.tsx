530892cada1880ed67fa221e0dd3c5e6f9b43453
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2024-11-09 01:49:59 -0300

    app-next-dalyGames

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -7,11 +7,11 @@ import { LiaGamepadSolid } from 'react-icons/lia';
 
 export function Header() {
   return (
-    <header className="w-full h-28 bg-slate-200 text-black px-2">
+    <header className="w-full h-24 bg-slate-200 text-black px-2">
 
       <div className="max-w-screen-xl mx-auto flex justify-center items-center h-28 sm:justify-between">
         
-        <nav className="flex justify-center items-center gap-4">
+        <nav className="flex justify-center items-center gap-4 font-semibold">
           <Link href='/'>
             <Image
               src={logoImg}
