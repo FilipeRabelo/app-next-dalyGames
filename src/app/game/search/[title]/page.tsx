363eb626babffe424c2023cf5ea68e1f8f56ec5d
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2024-11-12 16:15:45 -0300

    app-next-dalyGames

--- src/app/game/search/[title]/page.tsx
+++ src/app/game/search/[title]/page.tsx
@@ -19,11 +19,16 @@ async function getData(title: string) {
 
 
 
-export default async function Search({
-  params: { title }
-}: {
-  params: { title: string }
-}) {
+export default async function Search(
+  props: {
+    params: Promise<{ title: string }>
+  }
+) {
+  const params = await props.params;
+
+  const {
+    title
+  } = params;
 
   // resultado esta dentro da variavel games
   const games: GameProps[] = await getData(title);  // passando a requisicao para a variavel
@@ -54,5 +59,4 @@ export default async function Search({
       </Container>
     </main>
   )
-
 }
